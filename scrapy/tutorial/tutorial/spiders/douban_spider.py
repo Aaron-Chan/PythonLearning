6bda0d6ee6435a2918700f8d291e30b8b8e82484
chenxiaocheng <chenxc@zhongbosoft.com>
2017-10-11 20:43:31 +0800

    get all url of top250 movies

--- scrapy/tutorial/tutorial/spiders/douban_spider.py
+++ scrapy/tutorial/tutorial/spiders/douban_spider.py
@@ -5,7 +5,9 @@ from  scrapy import Request
 
 class douban_spider(scrapy.Spider):
     base_url = 'https://movie.douban.com/top250'
+    base_movie_url = 'https://movie.douban.com'
     start_urls = [base_url]
+    movie_urls = []
     name = 'douban'
 
     def parse(self, response):
@@ -14,11 +16,18 @@ class douban_spider(scrapy.Spider):
             title = item.xpath('//div/div[2]/div[1]/a/span[1]/text()').extract()
             rating_num = item.xpath('//div/div[2]/div[2]/div/span[2]/text()').extract()
             logging.debug('title %s rating num %s' % (title, rating_num))
+            url = self.base_movie_url + response.xpath('.//div/div[2]/div[1]/a/@href').extract()[0]
+            self.movie_urls.append(url)
 
         next_btn = response.xpath('//*[@id="content"]/div/div[1]/div[2]/span[3]')
         logging.debug(next_btn.xpath('.//a/@href').extract())
-
         nextUrl = self.base_url + next_btn.xpath('.//a/@href').extract()[0]
 
         if (nextUrl):
             yield Request(nextUrl, callback=self.parse)
+        else:
+            for movie_url in self.movie_urls:
+                yield Request(movie_url, callback=self.pase_movie)
+
+    def parse(self, response):
+        pass
